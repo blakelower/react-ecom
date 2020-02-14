1d0d0fe959688b7e78e0fffe27beea7c6e6ef1ab
blakelower <blakelower1999@gmail.com>
2020-02-14 14:08:29 +0700

    style components for buttn navbar

--- react-ecom/src/component/navbar.js
+++ react-ecom/src/component/navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.png";
+import styled from "styled-components";
 
 export default class Navbar extends Component {
   render() {
@@ -17,12 +18,35 @@ export default class Navbar extends Component {
           </Link>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+//styled components
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--mainPink);
+  color: var(--mainLight);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: 0.5s ease-in-out;
+  &:hover {
+    background: var(--mainPink);
+    color: var(--mainLight);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
